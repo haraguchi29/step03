ae1f650d85b676e39289e4a874ef82772a411dd9
haraguchi29 <hrgc.a29@gmail.com>
2022-01-19 23:19:02 +0900

    PCのスタイル

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+
+// トップに戻る
+
+const pagetopBtn = document.querySelector('#page-top');
+pagetopBtn.addEventListener('click', () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+});
